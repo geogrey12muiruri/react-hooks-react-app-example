3689f633e8fe156d4b9e93544b679b5b7f909f4a
geogrey12muiruri <greggambugua@gmail.com>
2022-09-22 11:55:36 +0300

    this commit changes the h1 element of App.js and added TestComponent

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
+import ",/TestComponent";
 
 ReactDOM.render(<App />, document.getElementById("root"));
